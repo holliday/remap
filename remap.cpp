9f4e521fb3450d45ebb9489dc18fa32d4c7a7860
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2015-01-11 00:29:21 -0500

    Initial bits for getKeyState

--- remap.cpp
+++ remap.cpp
@@ -9,6 +9,13 @@ DLLCLBK void InitModule(HINSTANCE hModule)
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+static inline bool getKeyState(int vkCode)
+{
+    SHORT value = GetAsyncKeyState(vkCode);
+    return value && (1 << (8 * sizeof(SHORT) - 1 ));
+}
+
+////////////////////////////////////////////////////////////////////////////////
 Remap::Remap(HINSTANCE hDLL):
     oapi::Module(hDLL)
 {
@@ -22,4 +29,25 @@ Remap::~Remap()
 ////////////////////////////////////////////////////////////////////////////////
 void Remap::clbkPreStep(double simt, double simdt, double mjd)
 {
+    bool menu    = getKeyState(VK_MENU);
+    bool shift   = getKeyState(VK_SHIFT);
+    bool control = getKeyState(VK_CONTROL);
+
+    bool right   = getKeyState(VK_NUMPAD6);
+    bool left    = getKeyState(VK_NUMPAD4);
+    bool up      = getKeyState(VK_NUMPAD9);
+    bool down    = getKeyState(VK_NUMPAD3);
+    bool forward = getKeyState(VK_NUMPAD8);
+    bool back    = getKeyState(VK_NUMPAD2);
+
+    if(menu)    oapiWriteLog("ALT");
+    if(shift)   oapiWriteLog("SHIFT");
+    if(control) oapiWriteLog("CTRL");
+
+    if(right)   oapiWriteLog("RIGHT");
+    if(left)    oapiWriteLog("LEFT");
+    if(up)      oapiWriteLog("UP");
+    if(down)    oapiWriteLog("DOWN");
+    if(forward) oapiWriteLog("FORWARD");
+    if(back)    oapiWriteLog("BACK");
 }
